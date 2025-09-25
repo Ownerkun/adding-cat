9a9cc8065236c87c7a8e4d5f5f6cbaaf4fdad628
Teerawat R. <tuiowner2004@gmail.com>
2025-09-25 20:29:33 +0700

    Add eas.json and update app.config.js for Supabase env vars

--- app.config.js
+++ app.config.js
@@ -2,9 +2,17 @@ import "dotenv/config";
 
 export default {
   expo: {
+    name: "Adding Cat",
+    slug: "adding-cat",
     extra: {
+      eas: {
+        projectId: "301a44b0-4edc-43fd-8c33-7029841c4107",
+      },
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    },
+    android: {
+      package: "com.ownerkun.addingcat",
     },
   },
 };
